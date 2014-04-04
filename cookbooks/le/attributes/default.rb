870328fee6c6a325ce60cbbba96cfba96bd970b1
Joe Heung <joe@joeheung.com>
2014-04-04 13:10:02 +0100

    Updated to use attributes for logs and apikey

---
+++ cookbooks/le/attributes/default.rb
@@ -0,0 +1,5 @@
+default[:le][:userkey] = "YOUR_API_KEY_GOES_HERE"
+default[:le][:hostname] = "ey-"
+default[:le][:logs_to_follow] = ["/var/log/syslog","/var/log/messages"]
+
+# Read more at https://logentries.com/doc/engineyard/
--- cookbooks/le/recipes/configure.rb
+++ cookbooks/le/recipes/configure.rb
@@ -4,24 +4,14 @@
 #
 
 execute "le register --account-key" do
-  command "le register --account-key #{node[:le_api_key]} --name #{node[:applications].keys.first}"
+  command "le register --account-key #{node[:le][:userkey]} --name #{node[:le][:hostname]}"
   action :run
   not_if { File.exists?('/etc/le/config') } 
 end
 
-follow_paths = [
-  "/var/log/syslog",
-  "/var/log/auth.log",
-  "/var/log/daemon.log"
-]
-(node[:applications] || []).each do |app_name, app_info|
-  follow_paths << "/var/log/nginx/#{app_name}.access.log"
+class Chef::Recipe
+  include FollowLogs
 end
 
-follow_paths.each do |path|
-  execute "le follow #{path}" do
-    command "le follow #{path}"
-    ignore_failure true 
-    action :run
-  end
-end
+follow_logs()
+
